d98365d2aafaed0ae4e280c4dc195a65a180991d
Josep <josepbauza7@gmail.com>
2019-03-23 17:53:25 +0100

    Add Started Project
    Yep a littel later but at least is now upload

---
+++ GameProject/Assets/Scripts/Menu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    //public Button MyButton = null;
+    // Use this for initialization
+    /*void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}*/
+
+    public void SendToCargarPartida () {
+        SceneManager.LoadScene(1);
+    }
+    public void SendToOptionMenu() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void SendToNuevaPartida() {
+        SceneManager.LoadScene(3);
+    }
+
+    public void SendToMenu() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void SendToPartida() {
+        SceneManager.LoadScene(4);
+    }
+
+    public void SendToCrearPartida() { 
+        SceneManager.LoadScene(5);
+    }
+    public void Partida2()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void login()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
+
